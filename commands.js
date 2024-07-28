10a187bd3e99085351667d3f03ca862108e70076
awong310 <awong310@gatech.edu>
2024-07-27 20:42:11 -0400

    first commit

---
+++ commands.js
@@ -0,0 +1,9 @@
+help = [
+    'abouta         Who even is Allison?',
+    'projects       Some fun coding projects',
+    'skills         Is she even hireable?',
+    'hobbies        Life away from the computer',
+    'contact        Contact this very hireable person',
+    'resources      Acknowledgements in helping create this',
+    'clear          Erase my existence'
+]
