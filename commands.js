b4bf02ce8b63e14737857f75489e8258b77f0125
awong310 <awong310@gatech.edu>
2024-08-03 20:56:21 -0400

    help command

--- commands.js
+++ commands.js
@@ -1,22 +1,46 @@
 
-const commands = {};
+const old_commands = {};
 
 const font = 'Small';
+
+const help = "\n\tabouta         Who even is Allison?\n" +
+"\tprojects       Some fun coding projects\n" +
+"\tskills         Is she even hireable?\n" +
+"\thobbies        Life away from the computer\n" +
+"\tcontact        Please contact this very hireable person\n" +
+"\tresources      Acknowledgements in helping create this\n";
 
 figlet.defaults({ fontPath: 'https://unpkg.com/figlet/fonts/' });
 figlet.loadFont([font], ready);
 
-var term;
+// uhh it doesn't use this but...
+var term = $('body').terminal(commands, {
+    greetings: false,
+    checkArity: false,
+    onInit() {
+        this.echo(() => render(this, "Allison Wong"));
+    }
+});
+
+// also doesn't use this but I wanna...
+const commands = {
+    banner: function() {
+        term.echo(() => render(this, "Allison Wong"));
+    }
+};
 
 function ready() {
     term = $('body').terminal({
         banner: function() {
             this.echo(() => render(this, "Allison Wong"));
+        },
+        help: function() {
+            this.echo(help);
         }
     }, {
         greetings: false,
         onInit() {
-            this.echo(() => render(this, "Allison Wong"))
+            this.echo(() => render(this, "Allison Wong"));
         }
     });
 }
@@ -28,20 +52,10 @@ function render(term, text) {
         width: cols,
         whitespaceBreak: true
     });
-    return `Hi, I'm...\n${ascii}\n`;
+    return `Hi, I'm...\n${ascii}\nWelcome to my interactive web terminal.\nFor a list of commands, type 'help'.\n`;
 }
 
-help = [
-    "<br>",
-    'abouta         Who even is Allison?',
-    'projects       Some fun coding projects',
-    'skills         Is she even hireable?',
-    'hobbies        Life away from the computer',
-    'contact        Please contact this very hireable person',
-    'resources      Acknowledgements in helping create this',
-    'clear          Erase my existence',
-    "<br>"
-];
+
 
 var linkedin = "https://www.linkedin.com/in/allisonwong22";
 
