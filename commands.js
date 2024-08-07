c90ff45df5c3f5f51271cd029abeb4c34694863c
awong310 <awong310@gatech.edu>
2024-08-06 23:00:30 -0400

    formatting & some info

--- commands.js
+++ commands.js
@@ -9,19 +9,42 @@ const abouta = "\n\tHi, my name is Allison (or alli)!\n" +
 "\tWelcome to a mini project/portfolio all about me & my hobbies :)\n" +
 "\tI'm a software developer, ... blah blah blah\n";
 
+// add Notion Templates page when I get approved (if? hopefully?)
+// link github & list out projects & short descriptions & languages
+// should I add hobby projects?
 const projects = "\n\tCurrent code is still under construction... polishing some bits and bytes, but coming soon :)\n";
 
-const skills = "";
+const skills = "\n\tTechnical Skills\n" +
+"\t*\n" +
+"\t*\n" +
+"\t*\n" +
+"\t*\n" +
+"\n\tSoftwares\n" +
+"\t* Frontend: Svelte, Flutter, HTML, Tailwind CSS, Javascript, CSS\n" +
+"\t* Backend: Python, Java, Go, C\n" +
+"\t* Services: Docker, MySQL, FastAPI, Grafana, Photoview\n" +
+"\n\tSoft Skills\n" +
+"\t*\n" +
+"\t*\n" +
+"\t*\n" +
+"\t*\n";
 
 const hobbies = "\n\tHobbies:\n" +
-"\t* Reading!!\n" +
-"\t* Film Photography\n" +
-"\t* Traveling\n" +
+"\t* Reading!! (fantasy, historic fiction, bildungsroman, etc.)\n" +
+"\t* Film Photography (film instagram available @justafilmcam)\n" +
+"\t* Traveling -- all over the world with my favorite people :)\n" +
 "\t* Chinese Yo-yo (Diabolo)\n" +
 "\t* Taekwondo\n";
 
-const contact = "idk";
-
 const linkedin = "https://www.linkedin.com/in/allisonwong22";
+
+const github = "https://github.com/allisonw22";
+
+const contact = "\n\tContact Information:\n" +
+"\tEmail: awong310@gatech.edu\n" +
+"\tPhone: 240-701-2521\n" +
+"\tLinkedIn: " + linkedin + "\n" +
+"\tGitHub: " + github + "\n" +
+"\tBased out of Maryland (but always willing to travel!)\n";
 
 const resources = "";
